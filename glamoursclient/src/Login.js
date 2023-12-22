d1432cef042404da85b7c5c9d9915e6a7aca6ef0
devupawar <145994900+devupawar@users.noreply.github.com>
2023-12-22 07:40:08 +0000

    done

--- glamoursclient/src/Login.js
+++ glamoursclient/src/Login.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 import React, { useState } from 'react'
 import { Button, Form, FormControl, FormGroup, FormLabel } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom';
+import { login } from './ReduxWork/UserSlice';
+import { useDispatch } from 'react-redux';
 
 
 const Login = () => {
   const navi = useNavigate()
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatcher = useDispatch()
 
   const handleLogin = () => {
     const loginData = {
@@ -19,35 +22,36 @@ const Login = () => {
 
       .then((response) => {
         if (response.data.success) {
+          dispatcher(login(response.data.data))
           navi('/home')
           alert('Login successful');
         } else {
           alert('invalid credentials')
         }
-        
+
       })
       .catch((error) => {
-       
+
         console.error('Login failed', error);
         alert('Login failed');
       });
   };
- 
+
   return (
     <div>
-        <Form className='lform'>
-            <h1 className='fhead'>Login</h1>
-            <FormGroup className='formg'>
-                <FormLabel>Email</FormLabel>
-                <FormControl type='email' onChange={(e) => setEmail(e.target.value)} placeholder='abc@gmail.com'></FormControl>
-            </FormGroup>
-            <FormGroup className='formg'>
-                <FormLabel>Password</FormLabel>
-                <FormControl type='password' onChange={(e) => setPassword(e.target.value)} placeholder='*********'></FormControl>
-            </FormGroup>
-            <Button onClick={handleLogin}  className='bt'>Login</Button>
-            <p>Not Regestered?</p><p onClick={()=>{navi('/register')}} className='navi'>Register</p>
-        </Form>
+      <Form className='lform'>
+        <h1 className='fhead'>Login</h1>
+        <FormGroup className='formg'>
+          <FormLabel>Email</FormLabel>
+          <FormControl type='email' onChange={(e) => setEmail(e.target.value)} placeholder='abc@gmail.com'></FormControl>
+        </FormGroup>
+        <FormGroup className='formg'>
+          <FormLabel>Password</FormLabel>
+          <FormControl type='password' onChange={(e) => setPassword(e.target.value)} placeholder='*********'></FormControl>
+        </FormGroup>
+        <Button onClick={handleLogin} className='bt'>Login</Button>
+        <p>Not Regestered?<span onClick={() => { navi('/register') }} className='navi'>Register</span></p>
+      </Form>
     </div>
   )
 }
--- glamoursclient/src/Profile.js
+++ glamoursclient/src/Profile.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from './ReduxWork/UserSlice'
+import { Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 const Profile = () => {
+  const navi = useNavigate()
+  const { UserData } = useSelector((state) => state.user)
+  const dispatcher = useDispatch()
+  console.log(UserData)
   return (
-    <div>Profile</div>
+
+    <div className='Ppanel'>
+      <h1 className='phead'>Your Profile</h1>
+      <h4 className='pdata'>Customer Name :  {UserData.CustomerName}</h4>
+      <h4 className='pdata'>Customer Email : {UserData.CustomerEmail}</h4>
+      <h4 className='pdata'>Customer MobileNo : {UserData.CustomerMobile}</h4>
+      <h4 className='pdata'>Customer Address : {UserData.CustomerAddress}</h4>
+      <h4 className='pdata'>Customer Password : {UserData.CustomerPassword}</h4>
+      <Button onClick={() => {
+        dispatcher(logout())
+        navi('/login')
+      }} className='pbt'>Logout</Button>
+    </div>
   )
 }
 
