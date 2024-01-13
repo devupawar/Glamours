17683c89b114873f6b374d63fcab8e47da574956
devupawar <145994900+devupawar@users.noreply.github.com>
2024-01-13 12:54:53 +0530

    done

--- glamoursAdmin/src/Dashboard.js
+++ glamoursAdmin/src/Dashboard.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Dashboard = () => {
-  const[custcount]
+  const[custcount,setcustcount]=useState(0)
+  const[AppointmentCount,setAppointmentCount]=useState(0)
+  const[serviceCount,setserviceCount]=useState(0)
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/api/getCustomerCount')
+    .then((result) => {
+      setcustcount(result.data.count)
+    }).catch((err) => {
+      
+    });
+
+    axios.get('http://localhost:5000/api/getservicecount')
+    .then((result) => {
+      setAppointmentCount(result.data.count)
+    }).catch((err) => {
+      
+    });
+
+    axios.get('http://localhost:5000/api/AppointmentCount')
+    .then((result) => {
+      setserviceCount(result.data.count)
+    }).catch((err) => {
+      
+    });
+  })
   return (
-    <div></div>
+    <div>
+     <h1> {custcount}</h1>
+     <h2>{serviceCount}</h2>
+    </div>
+
+    
   )
 }
 
